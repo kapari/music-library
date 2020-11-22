a65fcd3bfdaeff76bc0ea030aab5269af7adcb87
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2020-11-22 11:27:49 -0800

    add drag and drop for first column

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import Header from './components/Header';
 import AlbumItem from './components/AlbumItem';
 import styled from '@emotion/styled';
@@ -20,14 +21,33 @@ const UnshelvedList = styled.ul`
 `;
 
 function App() {
+  const initialColumnsState = {
+    unshelved: {
+      name: 'Unshelved Music',
+      orderedIds: []
+    }
+  }
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [collection, setCollection] = useState([]);
   const [lastPage, setLastPage] = useState(null);
-  const [totalItems, setTotalItems] = useState(null);
+  const [allData, setAllData] = useState({});
+  const [columns, setColumns] = useState(initialColumnsState);
 
+  // From API array to object with ids as keys
+  const transformData = (dataArr) => {
+    console.log('dataArr', dataArr)
+    let dataObj = {};
+    dataArr.forEach(item => {
+      dataObj[item.id] = item.basic_information;
+    });
+    console.log('dataObj', dataObj)
+    return dataObj;
+  }
+
+  // Get a page of data
   useEffect(() => {
+    setError(false);
     fetch(`https://api.discogs.com/users/blacklight/collection/folders/0/releases?page=${currentPage}&per_page=50`, 
       {
         method: 'GET',
@@ -38,18 +58,83 @@ function App() {
     )
       .then(response => response.json())
       .then(json => {
-        setCollection(json.releases);
+        const rawAlbumList = json.releases;
+        const transformedData = transformData(rawAlbumList)
         setLastPage(json.pagination?.pages);
-        setTotalItems(json.pagination?.items);
+        setAllData(prevState => {
+          return {
+            ...prevState,
+            ...transformedData
+          };
+        })
+        const newUnshelved = {
+          ...columns['unshelved'],
+          orderedIds: rawAlbumList.map(item => item.id)
+        }
+        setColumns(prevState => {
+          return {
+            ...prevState,
+            'unshelved': {
+              ...newUnshelved
+            }
+          }
+        })
         setIsLoaded(true);
-      }, (error) => {
-        console.log(error)
       })
-      .catch(error => console.log(`Something went wrong: ${error}`));
+      .catch(error => {
+        setError(true)
+        console.log(`Something went wrong: ${error}`)
+      });
   }, [currentPage])
 
-  const currentItems = collection.map((item) => {
-    return (<AlbumItem key={item.id} info={item.basic_information} />)
+  const isSameList = (source, destination) => {
+    return source.droppableId === destination.droppableId
+  }
+
+  const updateListContent = useCallback((result) => {
+    // Update list order
+    const { draggableId, source, destination } = result;
+    const startListId = source.droppableId;
+    const startListIndex = source.index;
+    const endListId = destination?.droppableId;
+    const endListIndex = destination?.index;
+
+    // If no destination, do nothing
+    if (!destination) {
+      return;
+    }
+    // If destination same as source, do nothing
+    if (
+      isSameList(source, destination) && 
+      source.index === destination.index
+    ) {
+      return;
+    }
+    // Update source column orderedIds
+    const newOrderedIds = Array.from(columns[source.droppableId].orderedIds);
+    newOrderedIds.splice(source.index, 1);
+    if (isSameList(source, destination)) {
+      newOrderedIds.splice(destination.index, 0, draggableId);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...columns[source.droppableId],
+          orderedIds: newOrderedIds
+        }
+      })
+    } else {
+      // TODO
+    }
+  })
+
+  const unshelvedItems = columns['unshelved'].orderedIds.map((id, index) => {
+    return (
+      <AlbumItem
+        key={id}
+        index={index}
+        info={allData[id]}
+      />
+    )
   })
 
   return (
@@ -58,9 +143,21 @@ function App() {
       <Main>
         <div>
           <h2>Unshelved Music</h2>
-          <UnshelvedList>
-            { currentItems }
-          </UnshelvedList>
+          <DragDropContext
+            onDragEnd={updateListContent}
+          >
+            <Droppable droppableId="unshelved">
+              {(provided) => (
+                <UnshelvedList 
+                  ref={provided.innerRef} 
+                  {...provided.droppableProps}
+                >
+                  {unshelvedItems}
+                  {provided.placeholder}
+                </UnshelvedList>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
       </Main>
     </Page>
--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import styled from '@emotion/styled';
 
 const Element = styled.li`
@@ -36,7 +37,7 @@ const DataItem = styled.li`
   }
 `;
 
-function AlbumItem({info}) {
+function AlbumItem({info, index}) {
   const artists = info.artists.map(item => {
     return <DataItem>{item.name}</DataItem>
   })
@@ -48,16 +49,29 @@ function AlbumItem({info}) {
   })
 
   return (
-    <Element>
-      <Title>{info.title}</Title>
-      <p>{info.year ? info.year : 'Unknown'}</p>
-      <DataHeading>Artists</DataHeading>
-      <DataList>{artists}</DataList>
-      <DataHeading>Labels</DataHeading>
-      <DataList>{labels}</DataList>
-      <DataHeading>Formats</DataHeading>
-      <DataList>{formats}</DataList>
-    </Element>
+    <Draggable
+      key={info.id}
+      draggableId={info.id.toString()} 
+      index={index}
+    >
+      {provided => (
+        <Element
+          key={info.id}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <Title>{info.title}</Title>
+          <p>{info.year ? info.year : 'Unknown'}</p>
+          <DataHeading>Artists</DataHeading>
+          <DataList>{artists}</DataList>
+          <DataHeading>Labels</DataHeading>
+          <DataList>{labels}</DataList>
+          <DataHeading>Formats</DataHeading>
+          <DataList>{formats}</DataList>
+        </Element>
+      )}
+    </Draggable>
   );
 }
 
