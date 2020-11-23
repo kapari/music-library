a837eae72b04291bd28db32c397a1fece83fde89
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2020-11-22 22:01:25 -0800

    make shelf name editable, style album card

--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic, faTag, faCompactDisc, faMicrophoneAlt } from '@fortawesome/free-solid-svg-icons'
 import { Draggable } from 'react-beautiful-dnd';
 import styled from '@emotion/styled';
 
 const Element = styled.li`
   margin-bottom: 10px;
-  border: 1px solid #ccc;
+  max-width: 100%;
+  width: 250px;
+  border-radius: 5px;
   background-color: #444;
-  padding: 10px;
+  padding: 5px 15px 15px;
+`;
+
+const Row = styled.div``;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Title = styled.h3`
   margin-top: 0;
-  margin-bottom: 0.5em;
+  margin-bottom: 0;
+`;
+
+const TitleText = styled.span`
+  margin-left: 0.5em;
 `;
 
 const DataHeading = styled.h4`
   display: inline-block;
   margin-top: 0;
   margin-bottom: 0.25em;
+  margin-right: 0.5em;
 `;
 
 const DataList = styled.ul`
+  display: inline;
   margin-left: 0;
   margin-bottom: 0.25em;
   padding: 0;
@@ -60,14 +78,34 @@ function AlbumItem({info, index}) {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <Title>{info.title}</Title>
-          <p>{info.year ? info.year : 'Unknown'}</p>
-          <DataHeading>Artists</DataHeading>
-          <DataList>{artists}</DataList>
-          <DataHeading>Labels</DataHeading>
-          <DataList>{labels}</DataList>
-          <DataHeading>Formats</DataHeading>
-          <DataList>{formats}</DataList>
+          <Header>
+            <Title>
+              <FontAwesomeIcon icon={faMusic} />
+              <TitleText>{info.title}</TitleText>
+            </Title>
+            <p>{info.year ? info.year : 'Unknown'}</p>
+          </Header>
+          <Row>
+            <DataHeading>
+              <FontAwesomeIcon icon={faMicrophoneAlt} fixedWidth />
+              <span className="sr-only">Artists</span>
+            </DataHeading>
+            <DataList>{artists}</DataList>
+          </Row>
+          <Row>
+            <DataHeading>
+              <FontAwesomeIcon icon={faTag} fixedWidth />
+              <span className="sr-only">Labels</span>
+            </DataHeading>
+            <DataList>{labels}</DataList>
+          </Row>
+          <Row>
+            <DataHeading>
+              <FontAwesomeIcon icon={faCompactDisc} fixedWidth />
+              <span className="sr-only">Formats</span>
+            </DataHeading>
+            <DataList>{formats}</DataList>
+          </Row>
         </Element>
       )}
     </Draggable>
--- src/components/ShelfItem.js
+++ src/components/ShelfItem.js
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import styled from '@emotion/styled';
 import AlbumList from './AlbumList.js';
 
 const Element = styled.li`
   margin-bottom: 10px;
-  border: 1px solid #ccc;
-  background-color: #444;
+  border: 1px solid #777;
+`;
+
+const Header = styled.header`
+  display: flex;
+  border-bottom: 1px solid #777;
   padding: 10px;
 `;
 
+const Heading = styled.h2`
+  margin: 0;
+  overflow: hidden;
+  width: 0;
+  height: 0;
+  opacity: 0;
+`;
+
+const Content = styled.div`
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+const SecretInput = styled.input`
+  transition: border-bottom 0.25s ease-in-out;
+  height: 40px;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid transparent;
+  background-color: transparent;
+  font-size: 20px;
+  color: white;
+  &:hover,
+  &:active,
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid white;
+  }
+`;
+
 function ShelfItem({ id, children }) {
+  const [name, setName] = useState('My New Shelf');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
   return (
     <Element key={id}>
-      <h2>My New Shelf</h2>
-      <Droppable droppableId={id}>
-        {(provided) => (
-          <AlbumList
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {children}
-            {provided.placeholder}
-          </AlbumList>
-        )}
-      </Droppable>
+      <Header>
+        <label>
+          <Heading>{name}</Heading>
+          <SecretInput type="text" value={name} onChange={handleNameChange} />
+        </label>
+      </Header>
+      <Content>
+        <Droppable droppableId={id} direction="horizontal">
+          {(provided) => (
+            <AlbumList
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {children}
+              {provided.placeholder}
+            </AlbumList>
+          )}
+        </Droppable>
+      </Content>
     </Element>
   );
 }
