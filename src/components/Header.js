1e4981879cbad4cbf2b78c7629f69322269dd468
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2020-11-24 21:52:01 -0800

    remove nav items

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,25 +11,11 @@ const Header = styled.header`
   padding: 20px 50px;
 `;
 
-const NavLink = styled.a`
-  display: inline-block;
-  padding: 10px 5px;
-  color: white;
-  &:hover,
-  &:active,
-  &:focus {
-    color: white;
-  }
-`;
-
 function AppHeader() {
   return (
     <Header>
       <h1>Music Library</h1>
-      <nav>
-        <NavLink href="#tbd">My Account</NavLink>
-        <NavLink href="#tbd">Log Out</NavLink>
-      </nav>
+      <nav></nav>
     </Header>
   );
 }
